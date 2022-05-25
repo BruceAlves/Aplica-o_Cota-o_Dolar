f66da8a8bf8f7787f8f5d7859247254c77e90f67
BruceAlves <brucewallace144@gmail.com>
2022-05-25 06:45:50 -0300

    Add project files.

---
+++ Aplicação_Cotação_Dolar/CotacaoDolar.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicação_Cotação_Dolar
+{
+    public class CotacaoDolar
+    {
+        [JsonProperty("@odata.context")]
+        public string OdataContext { get; set; }
+        public List<Value> value { get; set; }
+    }
+
+    public class Value
+    {
+        public double cotacaoCompra { get; set; }
+        public double cotacaoVenda { get; set; }
+        public string dataHoraCotacao { get; set; }
+    }
+}
---
+++ Aplicação_Cotação_Dolar/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicação_Cotação_Dolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HttpClient cliente = new HttpClient();
+
+            HttpResponseMessage respostaHttp;
+
+            Console.WriteLine("Digite a Data para cotação MM-DD-YYYY");
+            string data = Console.ReadLine();
+
+            respostaHttp = cliente.GetAsync($"https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%27{data}%27&$top=100&$format=json").Result;
+
+            string respostaString = respostaHttp.Content.ReadAsStringAsync().Result;
+
+            CotacaoDolar  resposta = JsonConvert.DeserializeObject<CotacaoDolar>(respostaString);
+
+            Console.WriteLine($"Cotação Compra:{resposta.value[0].cotacaoCompra}");
+            Console.WriteLine($"Cotação Venda:{resposta.value[0].cotacaoVenda}");
+            Console.WriteLine($"Data da Cotação:{resposta.value[0].dataHoraCotacao}");
+
+            Console.ReadKey();
+        }
+    }
+}
